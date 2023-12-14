4ec230301d89a8ce4a73b9ef32e2a3e84eda8a0d
Aditya Raja Permana <arpermana@tugu.com>
2023-12-14 09:21:54 +0700

    Add project files.

---
+++ DemografiService/Controllers/DemografiController.cs
@@ -0,0 +1,139 @@
+﻿using DemografiService.Models;
+using DemografiService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemografiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemografiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DemografiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/<DemografiController>
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        //[Route("GetAllDemografi")]
+        public ResponseDemografi Get()
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponseDemografi responseDemografi = new ResponseDemografi();
+            IDemografi idemografi = new IDemografi();
+
+            try
+            {
+                responseDemografi = idemografi.GetDemografi(connection);
+            }
+            catch (Exception ex)
+            {
+                responseDemografi.MsgCode = 500;
+                responseDemografi.MsgInfo = "Internal Server Error";
+            }
+            
+
+            return responseDemografi;
+        }
+
+        // GET api/<DemografiController>/5
+        [HttpGet("{Id}")]
+        //[Authorize]
+        //[Route("GetDemografiById")]
+        public ResponseDemografi Get(int Id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponseDemografi responseDemografi = new ResponseDemografi();
+            IDemografi idemografi = new IDemografi();
+
+            try
+            {
+                responseDemografi = idemografi.GetDemografiById(connection, Id);
+            }
+            catch (Exception ex)
+            {
+                responseDemografi.MsgCode = 500;
+                responseDemografi.MsgInfo = "Internal Server Error";
+            }
+
+            return responseDemografi;
+        }
+
+        // POST api/<DemografiController>
+        [HttpPost]
+        //[Authorize(Roles = "Administrator")]
+        //[Route("InsertDemografi")]
+        public ResponseDemografi Post([FromBody] RequestDemografi insertDemografi)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponseDemografi responseDemografi = new ResponseDemografi();
+            IDemografi idemografi = new IDemografi();
+
+            try
+            {
+                responseDemografi = idemografi.InsertDemografi(connection, insertDemografi);
+            }
+            catch (Exception ex)
+            {
+                responseDemografi.MsgCode = 500;
+                responseDemografi.MsgInfo = "Internal Server Error";
+            }
+
+            return responseDemografi;
+        }
+
+        // PUT api/<DemografiController>/5
+        [HttpPut]
+        //[Authorize(Roles = "Administrator")]
+        public ResponseDemografi Put([FromBody] Demografi updateDemografi)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponseDemografi responseDemografi = new ResponseDemografi();
+            IDemografi idemografi = new IDemografi();
+
+            try
+            {
+                responseDemografi = idemografi.UpdateDemografi(connection, updateDemografi);
+            }
+            catch (Exception ex)
+            {
+                responseDemografi.MsgCode = 500;
+                responseDemografi.MsgInfo = "Internal Server Error";
+            }
+
+            return responseDemografi;
+        }
+
+        // DELETE api/<DemografiController>/5
+        [HttpDelete("{Id}")]
+        //[Authorize(Roles = "Administrator")]
+        public ResponseDemografi Delete(int Id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponseDemografi responseDemografi = new ResponseDemografi();
+            IDemografi idemografi = new IDemografi();
+
+            try
+            {
+                responseDemografi = idemografi.DeleteDemografi(connection, Id);
+            }
+            catch (Exception ex)
+            {
+                responseDemografi.MsgCode = 500;
+                responseDemografi.MsgInfo = "Internal Server Error";
+            }
+
+            return responseDemografi;
+        }
+    }
+}
---
+++ PhotoIDService/Controllers/PhotoIDController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PhotoIDService.Models;
+using PhotoIDService.Services;
+using System;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PhotoIDService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoIDController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public PhotoIDController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/<DemografiController>
+        [HttpGet]
+        //[Authorize]
+        //[Route("GetAllDemografi")]
+        public ResponsePhotoID Get()
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponsePhotoID ResponsePhotoID = new ResponsePhotoID();
+            IPhotoID IPhotoID = new IPhotoID();
+
+            try
+            {
+                ResponsePhotoID = IPhotoID.GetPhotoID(connection);
+            }
+            catch (Exception ex)
+            {
+                ResponsePhotoID.MsgCode = 500;
+                ResponsePhotoID.MsgInfo = "Internal Server Error";
+            }
+
+            return ResponsePhotoID;
+        }
+
+        // GET api/<DemografiController>/5
+        [HttpGet("{Id}")]
+        //[Authorize]
+        //[Route("GetDemografiById")]
+        public ResponsePhotoID Get(int Id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponsePhotoID ResponsePhotoID = new ResponsePhotoID();
+            IPhotoID IPhotoID = new IPhotoID();
+
+            try
+            {
+                ResponsePhotoID = IPhotoID.GetPhotoIDById(connection, Id);
+            }
+            catch (Exception ex)
+            {
+                ResponsePhotoID.MsgCode = 500;
+                ResponsePhotoID.MsgInfo = "Internal Server Error";
+            }
+
+            return ResponsePhotoID;
+        }
+
+        // POST api/<DemografiController>
+        [HttpPost]
+        //[Authorize(Roles = "Administrator")]
+        //[Route("InsertDemografi")]
+        public ResponsePhotoID Post([FromBody] RequestPhotoID insertPhotoID)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponsePhotoID ResponsePhotoID = new ResponsePhotoID();
+            IPhotoID IPhotoID = new IPhotoID();
+            
+
+            try
+            {
+                ResponsePhotoID = IPhotoID.InsertPhotoID(connection, insertPhotoID);
+            }
+            catch (Exception ex)
+            {
+                ResponsePhotoID.MsgCode = 500;
+                ResponsePhotoID.MsgInfo = "Internal Server Error";
+            }
+
+            return ResponsePhotoID;
+        }
+
+        // PUT api/<DemografiController>/5
+        [HttpPut]
+        //[Authorize(Roles = "Administrator")]
+        public ResponsePhotoID Put([FromBody] PhotoID updatePhotoID)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponsePhotoID ResponsePhotoID = new ResponsePhotoID();
+            IPhotoID IPhotoID = new IPhotoID();
+            
+            try
+            {
+                ResponsePhotoID = IPhotoID.UpdatePhotoID(connection, updatePhotoID);
+            }
+            catch (Exception ex)
+            {
+                ResponsePhotoID.MsgCode = 500;
+                ResponsePhotoID.MsgInfo = "Internal Server Error";
+            }
+
+            return ResponsePhotoID;
+        }
+
+        // DELETE api/<DemografiController>/5
+        [HttpDelete("{Id}")]
+        //[Authorize(Roles = "Administrator")]
+        public ResponsePhotoID Delete(int Id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DbConnection").ToString());
+            ResponsePhotoID ResponsePhotoID = new ResponsePhotoID();
+            IPhotoID IPhotoID = new IPhotoID();
+
+            try
+            {
+                ResponsePhotoID = IPhotoID.DeletePhotoID(connection, Id);
+            }
+            catch (Exception ex)
+            {
+                ResponsePhotoID.MsgCode = 500;
+                ResponsePhotoID.MsgInfo = "Internal Server Error";
+            }
+
+            return ResponsePhotoID;
+        }
+    }
+}
